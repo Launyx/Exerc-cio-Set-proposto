afb17cf2bf77454cfc83617f29bd7e875cbc258e
Launyx <lunyx.castelhano@gmail.com>
2024-05-06 15:31:03 -0300

    Simplificando o exercício

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,22 +4,21 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-import entities.Student;
-
 public class Program {
     public static void main(String[] args) {
         
         Scanner tec = new Scanner(System.in);
-
         // Utilizando HashSet pois a ordem não importa
-        Set<Student> set = new HashSet<>();
+        Set<Integer> A = new HashSet<>();
+        Set<Integer> B = new HashSet<>();
+        Set<Integer> C = new HashSet<>();
 
         System.out.print("How many students for course A? ");
         int num = Integer.parseInt(tec.nextLine());
 
         for (int i = 0; i < num; i++){
-            int code = Integer.parseInt(tec.nextLine());
-            set.add(new Student(code));
+            int number = Integer.parseInt(tec.nextLine());
+            A.add(number);
 
         }
 
@@ -27,23 +26,25 @@ public class Program {
         num = Integer.parseInt(tec.nextLine());
 
         for (int i = 0; i < num; i++){
-            int code = Integer.parseInt(tec.nextLine());
-            set.add(new Student(code));
+            int number = Integer.parseInt(tec.nextLine());
+            B.add(number);
 
         }
-
 
         System.out.print("How many students for course C? ");
         num = Integer.parseInt(tec.nextLine());
 
         for (int i = 0; i < num; i++){
-            int code = Integer.parseInt(tec.nextLine());
-            set.add(new Student(code));
+            int number = Integer.parseInt(tec.nextLine());
+            C.add(number);
 
         }
 
-        System.out.println("Total students: " + set.size());
+        Set<Integer> total = new HashSet<>(A);
+        total.addAll(B);
+        total.addAll(C);
 
+        System.out.println("Total students: " + total.size());
 
         tec.close();
     }
